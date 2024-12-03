97d165f89d1d713d1a3b860573961db5a94549f8
amira <amira7assan4444@gmail.com>
2024-12-03 10:55:08 +0200

    appPar

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,11 +1,73 @@
 import 'package:flutter/material.dart';
-class Homepage extends StatelessWidget {
+
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: const Text('Welcome to the Coffee Shop!', style: TextStyle(fontWeight: FontWeight.bold)),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // Implement search functionality
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              // Implement shopping cart functionality
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'Welcome!',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey, // Set the background color to grey
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.black,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home,color: Colors.black),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search,color: Colors.black),
+            label: 'Category',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart,color: Colors.black),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person,color: Colors.black),
+            label: 'Me',
+          ),
+        ],
+      ),
     );
   }
 }
